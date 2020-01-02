f6310f3f5a65a17d256de60708731bf76b79b2d0
ingvarChuniak <i.chunyak@gmail.com>
2020-01-03 00:24:48 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route, 
+  Link, 
+  Redirect} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
 }
 
-export default App;
+const Public = () => <h3>Public</h3>;
+const Protected = () => <h3>Protected</h3>;
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+  return <Route {...rest} render={(props) => {
+      return fakeAuth.isAuthenticated ? <Component {...props}/> : <Redirect to="/login"/>
+    }}/>
+}
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Login</h3>
+      </div>
+    )
+  }
+}
+
+export default function AuthExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/public">Public Page</Link></li>
+          <li><Link to="/protected">Protected Page</Link></li>
+        </ul>
+        <Route path="/public" component={Public}></Route>
+        <Route path="/login" component={Login}></Route>
+        <PrivateRoute path="/protected" component={Protected}/>
+      </div>
+    </Router>
+  )
+}
